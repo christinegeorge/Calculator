b5bfda1e9c9a5644c3e9512cf78d8d420b4b09cc
Christine <mchristinegeorge@gmail.com>
2022-02-23 10:28:43 +0530

    for jenkins

--- src/com/demo/calculator/CalculatorExec.java
+++ src/com/demo/calculator/CalculatorExec.java
@@ -3,8 +3,17 @@ package com.demo.calculator;
 public class CalculatorExec {
 
 	public static void main(String []args) {
-		Calculator calc = new Calculator();
-		calc.add(2, 4);
-		calc.subtract(5, 8);
+		add(2, 4);
+		subtract(5, 8);
+	}
+	
+	public static void add(int a, int b) {
+		int sum = a+b;
+		System.out.println("Sum is:"+sum);
+	}
+	
+	public static void subtract(int a, int b) {
+		int diff = a>b ? a-b:b-a;
+		System.out.println("Difference is:"+diff);
 	}
 }
