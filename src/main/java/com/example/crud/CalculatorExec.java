ad5a4615ac1c3de1364a12e6541cfe27254ddf8f
Christine <mchristinegeorge@gmail.com>
2022-02-23 10:53:04 +0530

    added mulitply

--- src/main/java/com/example/crud/CalculatorExec.java
+++ src/main/java/com/example/crud/CalculatorExec.java
@@ -5,6 +5,7 @@ public class CalculatorExec {
 	public static void main(String []args) {
 		add(2, 4);
 		subtract(5, 8);
+		multiply(2, 10);
 	}
 	
 	public static void add(int a, int b) {
@@ -15,5 +16,10 @@ public class CalculatorExec {
 	public static void subtract(int a, int b) {
 		int diff = a>b ? a-b:b-a;
 		System.out.println("Difference is:"+diff);
+	}
+	
+	public static void multiply(int a, int b) {
+		int prod = a*b;
+		System.out.println("Product is:"+prod);
 	}
 }
